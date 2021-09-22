8bc0a47492b6fb6fccf3388f08fd20ceb5d26ef3
JRigs-QU22 <jtrigsby@quinnipiac.edu>
2021-09-22 12:21:02 -0400

    Additions
    - New Interaction: Focus
    
    Plyers must pick a task and while their meter fills, the other one falls. Players must balance the meters. Future integration plans. Include visual bar of focus and tie it into an energy mechanic to affect player effectiveness.

---
+++ Assets/FocusMeters.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FocusMeters : MonoBehaviour
+{
+    public bool isWorking;
+    public bool isWatching;
+
+    public int WorkFocus;
+    public int VideoFocus;
+
+    public float Countdown;
+
+    public Text WorkText;
+    public Text VideoText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Countdown = 1f;
+        WorkFocus = 50;
+        VideoFocus = 50;
+
+        isWorking = false;
+        isWatching = false;
+
+        WorkText.text = "Work Forcus: " + WorkFocus;
+        VideoText.text = "Video Focus: " + VideoFocus;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isWorking)
+        {
+            
+            Countdown -= Time.deltaTime;
+            if (Countdown < 0)
+            {
+                WorkFocus = WorkFocus + 2;
+                VideoFocus = VideoFocus - 1;
+                Countdown = 1f;
+               // Debug.Log(WorkFocus);
+               // Debug.Log(VideoFocus);
+            }
+        }
+        else if (isWatching)
+        {
+            
+            Countdown -= Time.deltaTime;
+            if (Countdown < 0)
+            {
+                WorkFocus = WorkFocus - 1;
+                VideoFocus = VideoFocus + 2;
+                Countdown = 1f;
+               // Debug.Log(WorkFocus);
+                //Debug.Log(VideoFocus);
+            }
+        }
+        if (WorkFocus == 0 || VideoFocus == 0)
+        {
+            Debug.Log ("You Lose");
+        }
+
+        WorkText.text = "Work Forcus: " + WorkFocus;
+        VideoText.text = "Video Focus: " + VideoFocus;
+    }
+
+    public void Working()
+    {
+        //if (gameObject.tag == "Work")
+        isWorking = true;
+        isWatching = false;
+
+    }
+
+    public void Playing()
+    {
+        //if (gameObject.tag == "Work")
+        
+            isWorking = false;
+            isWatching = true;
+        
+
+    }
+}
